10b4ed3e097e09d500ad7ae812fcdb75c13b596d
LumaLux <LumaLux@bitbucket.org>
2019-01-19 16:42:13 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server log.');
+        }
+    });
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: "We'll be right back",
+//     });
+// });
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Homepage',
+        welcomeMsg: 'Welcome to the site.',
+    })
+
+});
+
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error handling request.'
+    });
+});
+
+app.listen(80, () => {
+    console.log('Server is up on port 80');
+});
