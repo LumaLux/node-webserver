a8c7f6d2050a60d6fd35fb0529f27a6e07aaf7fc
LumaLux <LumaLux@bitbucket.org>
2019-01-19 18:16:40 +0100

    third commit

--- server.js
+++ server.js
@@ -45,7 +45,12 @@ app.get('/', (req, res) => {
 
 });
 
-
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: "Projects",
+        message: 'This is the portfolio page',
+    });
+});
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
